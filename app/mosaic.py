550a6678c62580b94f6ee93262c7b142db2576cc
Adam <adelionek@gmail.com>
2019-04-13 21:26:16 +0200

    added save images function - works. Saving in app folder

--- app/mosaic.py
+++ app/mosaic.py
@@ -7,11 +7,5 @@ urls = []
 def save_images():
     for i in range(arguments['ile']):
         url = requests.get(urls[i])
-        with open('zdj_%d' % i, 'wb') as f:
-            f.write(url.content)
-
-
-    for url_address in urls:
-        url = requests.get(url_address)
-        with open('zdj_%d' % i, 'wb') as f:
+        with open('zdj_%d.jpeg' % i, 'wb') as f:
             f.write(url.content)
